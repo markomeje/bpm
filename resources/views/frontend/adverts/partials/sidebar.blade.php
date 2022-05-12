acf5369755d50d64dfcbfdccc0875139c9e576f9
markomeje <markomejeonline@gmail.com>
2022-05-12 16:47:14 +0100

    Remove ad sidebar duplicate size

--- resources/views/frontend/adverts/partials/sidebar.blade.php
+++ resources/views/frontend/adverts/partials/sidebar.blade.php
@@ -1,15 +1,3 @@
-@set('ssbs', \App\Models\Advert::where(['status' => 'active', 'size' => 'ssb'])->inRandomOrder()->take(2)->get())
-@if(!empty($ssbs))
-	<div class="row">
-		@foreach($ssbs as $advert)
-			<div class="col-6 mb-4">
-				<a href="{{ $advert->link }}" target="_blank" class="w-100" style="height: 200px;">
-					<img src="{{ empty($advert->image) ? '/images/banners/placeholder.png' : $advert->image->link }}" class="img-fluid border w-100 h-100 object-cover">
-				</a>
-			</div>
-		@endforeach
-	</div>
-@endif
 @set('svbs', \App\Models\Advert::where(['status' => 'active', 'size' => 'svb'])->inRandomOrder()->take(2)->get())
 @if(!empty($svbs))
 	<div class="row">
